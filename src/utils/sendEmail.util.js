48da22765f34333e68fdd45cebcaee3c88b6ac59
Yehia Mohamed <yehiamohamed915@yahoo.com>
2025-10-21 00:58:15 +0300

    - Adding Resend package - Create sendEmail using resend function and called it inside register function

--- src/utils/sendEmail.util.js
+++ src/utils/sendEmail.util.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+import { Resend } from "resend";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
@@ -12,6 +13,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, html) => {
   try {
     console.log("Sending email to:", to);
@@ -24,6 +26,20 @@ export const sendEmail = async (to, subject, html) => {
     console.log("Email sent:", info.response);
   } catch (err) {
     console.error("Email send failed:", err);
+    throw err;
+  }
+};
+
+export const sendEmailUsingResend = async (to, subject, html) => {
+  try {
+    await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject,
+      html,
+    });
+  } catch (err) {
+    console.error("Email send failed From Resend:", err);
     throw err;
   }
 };
