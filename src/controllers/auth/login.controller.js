6360dc9ac47c4352617ae88586be2af39dc10908
abdelrahman <abdelrahmannn202@gmail.com>
2025-10-21 20:26:04 +0300

    returning isVerified with login

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -4,7 +4,7 @@ import asyncHandler from "express-async-handler";
 export const loginController = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const result = await loginService({ email, password });
-  
+
   res.status(200).json({
     message: "Login successful",
     token: result.token,
@@ -12,6 +12,7 @@ export const loginController = asyncHandler(async (req, res) => {
       id: result.user._id,
       userName: result.user.userName,
       email: result.user.email,
+      isVerified: result.user.isVerified,
     },
   });
 });
