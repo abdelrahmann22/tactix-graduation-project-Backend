76a9e383aad98d40955856c7dc050cfc5b6f7c01
abdelrahman <abdelrahmannn202@gmail.com>
2025-10-21 06:13:26 +0300

    deleting resend import

--- src/services/auth/register.service.js
+++ src/services/auth/register.service.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import { User } from "../../models/user.model.js";
 import { Token } from "../../models/token.model.js";
 import { generateToken, hashToken } from "../../utils/token.util.js";
-import { sendEmail, sendEmailUsingResend } from "../../utils/sendEmail.util.js";
+import { sendEmail } from "../../utils/sendEmail.util.js";
 import { CustomError } from "../../utils/customError.util.js";
 import { verifyEmailTemplate } from "../../utils/emailTemplates/verfiyEmailTemplate.js";
 export const registerService = async ({ userName, email, password }) => {
